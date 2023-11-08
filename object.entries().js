dacc352f05c3fb94a73eee8d0d794f7f41e8a9f3
Fahmida Nusrat Promy <76656865+fahmida246@users.noreply.github.com>
2023-11-08 16:22:26 +0600

    Update object.entries().js

--- object.entries().js
+++ object.entries().js
@@ -1,8 +1,29 @@
 let tr = Object.entries(car);
-console.log(tr);
+console.log(tr); // tr er result check
 let conver = tr.map((item)=>
 {
     let [a, b] = item;
     return a;
 });
-console.log(conver);
+console.log(conver);  // conver er result check
+
+//output
+//[
+//  [ 'id', 101 ],
+//  [ 'title', 'iphone 12' ],
+//  [ 'description', 'brilliant phone with 4k camera' ],
+//  ['price', 549 ],
+//  [ 'rating', 4.69 ],
+//  [ 'brand', 'Apple' ],
+ // [ 'discountpercentage', 20 ]
+//]  tr er output
+//
+//[
+ // 'id',
+ // 'title',
+ // 'description',
+ // 'price',
+ // 'rating',
+ // 'brand',
+ // 'discountpercentage'
+// ] conver er output
